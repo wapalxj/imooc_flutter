b662fda7d08b7e2012035d69363af997769eee6b
muguihai <wapalxj@163.com>
2021-10-30 16:50:10 +0800

    211030

--- lib/first/ui/page/page_article.dart
+++ lib/first/ui/page/page_article.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:imooc_flutter/first/http/api.dart';
 
 class ArticlePage extends StatefulWidget {
   const ArticlePage({Key? key}) : super(key: key);
@@ -13,7 +18,7 @@ class _ArticlePageState extends State<ArticlePage> {
   ScrollController _controller = ScrollController();
 
   ///控制正在加载的显示
-  bool _isLoading = true;
+  bool _isHide = true;
 
   ///请求到的文章数据
   List articles = [];
@@ -30,13 +35,25 @@ class _ArticlePageState extends State<ArticlePage> {
   void initState() {
     super.initState();
 
+    //上拉加载
+    _controller.addListener(() {
+      ///获取ScrollController 监听控件可以滚动的最大距离
+      var maxScroll = _controller.position.maxScrollExtent;
+
+      ///获得当前位置的像素值
+      var pixels = _controller.position.pixels;
+
+      ///当前滑动位置到达底部，加载更多
+      if (maxScroll == pixels && articles.length < listTotalSize) {
+
+        print("_getArticleList== listTotalSize== ${listTotalSize}");
+
+        _getArticleList();
+      }
+    });
+
     //初始请求数据
     _pullToRefresh();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack();
   }
 
   @override
@@ -45,18 +62,128 @@ class _ArticlePageState extends State<ArticlePage> {
     super.dispose();
   }
 
-  void _pullToRefresh() async {
+  Future<void> _pullToRefresh() async {
     curPage = 0;
+    //组合2个请求
     Iterable<Future> futures = [_getArticleList(), _getBanner()];
     await Future.wait(futures);
-    _isLoading = true;
+    _isHide = false;
+
+    //重新执行build()
+    setState(() {});
   }
 
-  Future<String> _getArticleList() async {
-    return "";
+  //请求列表
+  _getArticleList([bool update = true]) async {
+    //请求成功是map,失败是null
+    var data = await Api.getArticleList(0);
+
+    print("_getArticleList====0000000000000 ${data}");
+    if (data != null) {
+      var map = data['data'];
+      var datas = map['datas'];
+
+      //文章总数
+      listTotalSize = map['total'];
+
+      print("_getArticleList== listTotalSize== ${listTotalSize}");
+
+      if (curPage == 0) {
+        articles.clear();
+      }
+      curPage++;
+      articles.addAll(datas);
+
+      print("_getArticleList==== ${articles.length}");
+
+      //更新ui
+      if (update) {
+        //重新执行build()
+        setState(() {});
+      }
+    }
   }
 
-  Future<String> _getBanner() async {
-    return "";
+  _getBanner([bool update = true]) async {
+    var data = await Api.getBanner();
+    if (data != null) {
+      banners.clear();
+      banners.addAll(data['data']);
+      if (update) {
+        setState(() {});
+      }
+    }
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        //Offstage控件： 控制显示/隐的
+
+        ///正在加载
+        Offstage(
+          offstage: !_isHide, //显示loading
+          child: Center(child: CircularProgressIndicator()),
+        ),
+
+        ///内容
+        // Offstage(
+        //   offstage: _isHide, //显示内容
+        //   child: Center(child: Text("加载完成")),
+        // )
+        //
+
+        ///内容
+        Offstage(
+          offstage: _isHide, //显示内容
+          //下拉刷新
+          child: RefreshIndicator(
+              //Listview
+              child: ListView.builder(
+                itemCount: articles.length,
+                itemBuilder: (context, i) => _buildItem(i),
+                controller: _controller,
+              ),
+              onRefresh: _pullToRefresh),
+        )
+      ],
+    );
+  }
+
+  Widget _buildItem(int position) {
+    // return Text("item ==== $i in ${articles.length}");
+
+    // if (position == 0) {
+    //   //banner
+    //   return Container(height: 180, child: _bannerView());
+    // }
+
+    var color;
+    if (position % 2 == 0) {
+      color = Colors.green;
+    } else {
+      color = Colors.red;
+    }
+    return Container(
+        color: color,
+        height: 100,
+        child: Text("item ==== $position in ${articles.length}"));
+  }
+
+// Widget _bannerView() {
+//   var list = banners.map((item) {
+//     print("_bannerView====${item['imagePath']}");
+//     return Image.network(item['imagePath'], fit: BoxFit.cover);
+//   }).toList();
+//
+//   if (list.isEmpty) {
+//     print("_bannerView====list.isEmpty");
+//     return null;
+//   }
+//   return BannerView(
+//     list,
+//     intervalDuration: const Duration(seconds: 3),
+//   );
+// }
 }
